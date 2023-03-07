0634c2573359f8f82879e14e3b90589084f8454c
Kadir Furkan Gün <fkadirgun@gmail.com>
2023-03-07 04:40:03 +0300

    first commit

---
+++ Sayisal.java
@@ -0,0 +1,21 @@
+package Öğrenciler;
+
+
+public class Sayisal extends Aday{
+
+    public Sayisal(int turkce, int matematik, int edebiyat, int fizik, String isim) {
+        super(turkce, matematik, edebiyat, fizik, isim);
+        //TODO Auto-generated constructor stub
+    }
+
+    
+    @Override
+    public int puanHesapla() {
+    
+        return getTurkce() * 5 + getMatematik() * 5 + getFizik() * 4 + getEdebiyat() * 1;
+        
+    
+    }
+    
+}
+
